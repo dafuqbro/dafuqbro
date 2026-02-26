bf9fc3adcd3009eb86ed79cf30a72d9d704087c7
dafuqbro <h.yordanova16@gmail.com>
2026-02-26 11:43:13 +0200

    Create entry.server.tsx

---
+++ app/entry.server.tsx
@@ -0,0 +1,33 @@
+import type { EntryContext } from "react-router";
+import { ServerRouter } from "react-router";
+import { renderToReadableStream } from "react-dom/server";
+import { isbot } from "isbot";
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext
+) {
+  const userAgent = request.headers.get("user-agent");
+  const body = await renderToReadableStream(
+    <ServerRouter context={routerContext} url={request.url} />,
+    {
+      signal: request.signal,
+      onError(error: unknown) {
+        console.error(error);
+        responseStatusCode = 500;
+      },
+    }
+  );
+
+  if (userAgent && isbot(userAgent)) {
+    await body.allReady;
+  }
+
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
