f96b18d6773fd015971c348b6ec9ca9ea8fc4b4d
dafuqbro <h.yordanova16@gmail.com>
2026-02-26 11:34:23 +0200

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+  ],
+});
